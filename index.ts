7df750cab00527195ab2b7ec4081c7d191c24fd3
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-07-27 15:45:05 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
-
+let ez=new Array();
+for(let i =0;i<10;i++){
+  ez.push(i);
+}
+console.log(ez)
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
