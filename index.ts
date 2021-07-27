74c21cd15cf01d1a1fd8324539712c3efb9be8c5
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-07-27 16:59:31 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 // Import stylesheets
 import './style.css';
 let ez=new Array();
-for(let i =0;i<10;i++){
-  ez.push(i);
+for(let i =1;i<10;i++){
+  for(let j=0;j<10;j++){
+    let a=(i.toString()+"*"+ j.toString());
+    ez.push(a);
+  }
 }
 console.log(ez)
 // Write TypeScript code!
