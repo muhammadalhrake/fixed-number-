e93d8b03e5a2a62043839a1b57ab54005bc8688f
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-07-28 13:33:44 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,28 +2,21 @@
 import './style.css';
 let hard = new Array();
 let ez = new Array();
-
+let hardComplement = new Array();
+let ezComplement = new Array();
 function generatePoss(generation: string[], level: string, rule: string) {
-  let fN, sN, rang;
+  let fN=6, sN=6, rang=9;
   if (rule == 'tens') {
     if (level == 'ez') {
       fN = 1;
       sN = 0;
       rang = 5;
-    } else if (level == 'hard') {
-      fN = 6;
-      sN = 6;
-      rang = 9;
     }
   } else if (rule == 'single') {
     if (level == 'ez') {
       fN = 0;
       sN = 1;
       rang = 5;
-    } else if (level == 'hard') {
-      fN = 6;
-      sN = 6;
-      rang = 9;
     }
   }
   for (let i = fN; i <= rang; i++) {
@@ -45,7 +38,24 @@ function generatePoss(generation: string[], level: string, rule: string) {
   }
   return generation;
 }
-console.log(generatePoss(ez, 'ez', 'single'));
+function generateComplement(arrComplement:string[],level: string, rule: string){
+  let fN=5, sN=1, rang=9;
+  if (rule == 'tens') {
+    if (level == 'ez') {
+      fN = 1;
+      rang = 5;
+    }else{
+      fN=6
+    }
+  } else if (rule == 'single') {
+    if (level == 'ez') {
+      fN = 0;
+      rang = 4;
+    }
+  }
+
+}
+//console.log(generatePoss(ez, 'ez', 'single'));
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
