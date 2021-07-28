d349def7bbcaeceb059f85e754d48c02009e6b5c
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-07-28 16:22:19 +0300

    2 files modified

--- fixed-generation.ts
+++ fixed-generation.ts
@@ -103,3 +103,5 @@ export function generateComplement(
   }
   return arrComplement;
 }
+
+function 
--- index.ts
+++ index.ts
@@ -1,10 +1,45 @@
 // Import stylesheets
 import './style.css';
+// imports
+import { generatePoss } from './fixed-generation';
 let tensHard = new Array();
 let tensEz = new Array();
 let hardComplement = new Array();
 let ezComplement = new Array();
+let fixedProbability = {
+  tens: {
+    Complement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    },
+    unComplement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    }
+  },
+  single: {
+    Complement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    },
+    unComplement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    }
+  },
+  mixed: {
+    Complement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    },
+    unComplement: {
+      Easy: new Array(),
+      Difficult: new Array()
+    }
+  }
+};
 
+// console.log(generatePoss())
 //console.log(generateComplement(ezComplement, 'hard', 'single'));
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
